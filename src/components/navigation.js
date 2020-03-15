fdada0ae520cfa26d2995ff5af051aed582486fb
Midge 't Hoen <midgethoen@gmail.com>
2020-03-15 14:08:36 +0100

    fix nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,72 +1,88 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState, useCallback } from "react"
 
+const Navigation = ({ siteTitle, path }) => {
+  const [open, setOpen] = useState(false)
+  const toggle = useCallback(() => setOpen(!open))
 
+  console.log("render", open)
+  return (
+    <>
+      <nav className="topnav">
+        <div className="topnav-left">
+          <button className="js_closesidenav" onClick={toggle}>
+            O
+          </button>
+          <Link className="sitetitle" to="/">
+            {siteTitle}
+          </Link>
+        </div>
+        <div className="topnav-right">
+          <Link className="" to="/massageoverview">
+            Massages
+          </Link>
+          <Link className="" to="/skintreatmentoverview">
+            Skin Treatments
+          </Link>
+          <Link className="" to="/waxoverview">
+            Waxing
+          </Link>
+          <Link className="displaynone" to="/medical-conditions">
+            Medical Conditions
+          </Link>
+          <Link className="displaynone" to="/products">
+            Products
+          </Link>
+          <a className="" href="https://holistickiwishop.com/">
+            Webshop
+          </a>
+          <a
+            className="btn btn-primary"
+            href="https://www.fresha.com/providers/holistic-kiwi-agx16c7f"
+            type="button"
+          >
+            Book Now
+          </a>
+        </div>
+      </nav>
 
-const Navigation = ({ siteTitle, path }) => (
-  <>
-  <nav className="topnav js_sidenav-open">
-    <div className="topnav-left">
-      <button className="js_opensidenav">O</button>
-      <Link className="sitetitle" to="/">{siteTitle}</Link>
+      <nav className={`sidenav ${open ? "js_sidenav-open" : ""}`}>
+        <button className="js_closesidenav" onClick={toggle}>
+          close
+        </button>
+        <div className="mainnav">
+          <Link className="" to="/">
+            Home
+          </Link>
+          <Link className="" to="/massageoverview">
+            Massages
+          </Link>
+          <Link className="" to="/skintreatmentoverview">
+            Skin Treatments
+          </Link>
+          <Link className="" to="/waxoverview">
+            Waxing
+          </Link>
+        </div>
+        <div className="subnav">
+          <Link className="" to="/about-us">
+            About us
+          </Link>
+          <Link className="" to="/contact">
+            Contact
+          </Link>
+        </div>
 
-    </div>
-    <div className="topnav-right">
-
-      <Link className="" to="/massageoverview">Massages</Link>
-      <Link className="" to="/skintreatmentoverview">Skin Treatments</Link>
-      <Link className="" to="/waxoverview">Waxing</Link>
-      <Link className="displaynone" to="/medical-conditions">Medical Conditions</Link>
-      <Link className="displaynone" to="/products">Products</Link>
-      <a className="" href="https://holistickiwishop.com/">
-        Webshop
-      </a>
-      <a
-        className="btn btn-primary"
-        href="https://www.fresha.com/providers/holistic-kiwi-agx16c7f"
-        type="button"
-      >
-        Book Now
-      </a>
-
-    </div>
-  </nav>
-
-  <nav className="sidenav">
-    <button className="js_closesidenav">close</button>
-    <div className="mainnav">
-    <Link className="" to="/">Home</Link>
-      <Link className="" to="/massageoverview">Massages</Link>
-      <Link className="" to="/skintreatmentoverview">Skin Treatments</Link>
-      <Link className="" to="/waxoverview">Waxing</Link>
-
-    </div>
-    <div className="subnav">
-      <Link className="" to="/about-us">About us</Link>
-      <Link className="" to="/contact">Contact</Link>
-    </div>
-
-    <a
-      className="btn btn-primary"
-      href="https://www.fresha.com/providers/holistic-kiwi-agx16c7f"
-      type="button"
-    >
-      Book Now
-    </a>
-  </nav>
-
-
-
-</>
-)
-
-Navigation.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Navigation.defaultProps = {
-  siteTitle: ``,
+        <a
+          className="btn btn-primary"
+          href="https://www.fresha.com/providers/holistic-kiwi-agx16c7f"
+          type="button"
+        >
+          Book Now
+        </a>
+      </nav>
+    </>
+  )
 }
 
 export default Navigation
